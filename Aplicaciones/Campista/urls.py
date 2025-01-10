ac7fa8d2b6de442698957927210628b3803504cc
Stalinjs <137074336+Stalinjs@users.noreply.github.com>
2025-01-10 13:43:43 -0500

    Update urls.py

--- Aplicaciones/Campista/urls.py
+++ Aplicaciones/Campista/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),  # Página de inicio
-    path('inicio/', views.inicio, name='inicio'),
     path('campista/', views.nuevo_campista, name='nuevo_campista'),
     path('listadoCampista/', views.listar_campista, name='listar_campista'),
     path('guardarCampista/', views.guardar_campista, name='guardar_campista'),
